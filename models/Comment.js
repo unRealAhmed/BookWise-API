fa787bdb0cb5038d222c3a47516f34204c1b3a9c
Ahmed Sayed <therealahme@gmail.com>
2023-11-22 16:25:06 +0200

    build comment model

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  // Text of the comment
+  text: {
+    type: String,
+    required: [true, 'Text is required for a comment.'],
+    maxlength: [500, 'Comment text cannot exceed 500 characters'],
+  },
+
+  // Author information
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: {
+      type: String,
+      required: [true, 'Username of the author is required.'],
+    },
+  },
+
+  // Book information associated with the comment
+  book: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+    title: {
+      type: String,
+      required: [true, 'Title of the associated book is required.'],
+    },
+  },
+
+  // Date the comment was created
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment
